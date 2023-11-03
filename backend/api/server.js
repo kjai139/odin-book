3c41fd2d4dc0fb3671834b61d73688153ddeb056
kj y <therealkjai139@gmail.com>
2023-11-02 21:37:40 -0400

    start backend

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 4000
+const mongodb = process.env.MONGO_DB 
+const mongoose = require('mongoose')
+const cors = require('cors')
+const allowedOrigins = ['http://localhost:3000']
 
+const main = async () => {
+    try {
+        mongoose.connect(mongodb)
+        console.log('mongodb connected')
+    } catch (err) {
+        console.log(err)
+    }
+}
 
+main()
 
 app.listen(port, () => {
     console.log(`server running on ${port}`)
