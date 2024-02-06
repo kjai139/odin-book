6e8cdc517f544e868669ee722700a22f730044bc
kj139 <therealkjai139@gmail.com>
2024-02-05 23:53:03 -0500

    start user pages

--- frontend/src/app/user/[slug]/page.tsx
+++ frontend/src/app/user/[slug]/page.tsx
@@ -1,7 +1,10 @@
 
 import { User } from "../../../../interfaces/user.interface"
-import axiosInstance from '../../../../axios'
+
 import { notFound } from "next/navigation"
+import Image from "next/image"
+import { BsPersonCircle } from "react-icons/bs"
+import { formatUsername } from "@/app/_utils/formatStrings"
 
 async function fetchUser(id) {
     const res = await fetch(`http://localhost:4000/api/user/getPage/?id=${id}`)
@@ -24,9 +27,40 @@ export default async function UserDetails({ params }) {
     
     return (
         <>
-            <div>
-            <h1>USER DETAILS PAGE: {params.slug}</h1>
+        <div className="up-bg rounded shadow">
+        <div className="userpg-bg">
+        
+            <div className="flex flex-col">
+                <div className="userpf-cont">
+                    <h1>USER DETAILS PAGE: {params.slug}</h1>
+                </div>
+
+                <div className="flex border-b border-solid border-gray">
+                    <div className="rounded-full bg-white p-2 border-2 border-solid border-white">
+                        <div className="pfp-mask">
+                            <div className="rounded-full overflow-hidden w-125 h-125">
+                            {user.userInfo.image ?
+                            <Image src={user.userInfo.image} width={125} height={125} alt="user profile picture"></Image> :
+                            <BsPersonCircle className="backup-user-img" size={125}></BsPersonCircle> 
+                            }
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h1>{formatUsername(user.userInfo.name)}</h1>
+                    </div>
+                </div>
+                <div>
+                    <button>Post</button>
+                </div>
+                
+            
             </div>
+        </div>
+        </div>
+        <div className="up-content-cont">
+
+        </div>
         </>
     )
 }
